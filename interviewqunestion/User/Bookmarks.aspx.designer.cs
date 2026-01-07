2cad2d1eb22ecf675785dc9db0794f2cad139622
DarshanLila1315 <23020201093@darshan.ac.in>
2026-01-07 10:28:27 +0530

    all change from darshan

--- interviewqunestion/User/Bookmarks.aspx.designer.cs
+++ interviewqunestion/User/Bookmarks.aspx.designer.cs
@@ -15,15 +15,6 @@ namespace interview_questions.User
     {
 
         /// <summary>
-        /// lblBookmarkCount control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblBookmarkCount;
-
-        /// <summary>
         /// gvBookmarks control.
         /// </summary>
         /// <remarks>
